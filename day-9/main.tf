e776ae333b665006011ffd5934cb6236ba6caf97
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2026-07-12 17:12:49 +0300

    Completed day 9

---
+++ day-9/main.tf
@@ -0,0 +1,21 @@
+data "aws_instance" "instance" {
+  filter {
+    name   = "tag:Name"
+    values = [var.instance_name]
+  }
+}
+
+import {
+  id = data.aws_instance.instance.id
+  to = aws_instance.instance
+}
+
+resource "aws_instance" "instance" {
+  ami                     = data.aws_instance.instance.ami
+  instance_type           = data.aws_instance.instance.instance_type
+  disable_api_termination = true
+
+  tags = {
+    "Name" = var.instance_name
+  }
+}
